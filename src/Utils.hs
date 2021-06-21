aedd6814a2a0229c300acdda52bc5bde97a428ed
aleksandr <alexbromel@gmail.com>
2021-06-22 02:26:42 +0300

    init

---
+++ src/Utils.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# options_ghc -fno-warn-orphans          #-}
+{-# options_ghc -Wno-redundant-constraints #-}
+{-# options_ghc -fno-strictness            #-}
+{-# options_ghc -fno-specialise            #-}
+
+module Utils where
+
+
+import           Ledger
+import           Ledger.Value        (AssetClass (..), assetClass, assetClassValue, assetClassValueOf)
+import           Playground.Contract (FromJSON, Generic, ToJSON, ToSchema)
+import qualified PlutusTx
+import PlutusTx.Prelude
+    ( Bool,
+      Integer,
+      String,
+      (.),
+      ($),
+      Eq(..),
+      AdditiveGroup,
+      AdditiveMonoid,
+      AdditiveSemigroup,
+      MultiplicativeSemigroup,
+      Ord )
+import qualified Prelude             as Haskell
+import           Text.Printf         (PrintfArg)
+import qualified Data.ByteString.Char8  as C
+
+data CoinA = CoinA
+
+PlutusTx.makeIsDataIndexed ''CoinA [('CoinA, 0)]
+PlutusTx.makeLift ''CoinA
+
+data CoinB = CoinB
+
+PlutusTx.makeIsDataIndexed ''CoinB [('CoinB, 0)]
+PlutusTx.makeLift ''CoinB
+
+data LPToken = LPToken
+
+PlutusTx.makeIsDataIndexed ''LPToken [('LPToken, 0)]
+PlutusTx.makeLift ''LPToken
+
+deriving anyclass instance ToSchema AssetClass
+
+newtype Coin a = Coin { unCoin :: AssetClass }
+  deriving stock   (Haskell.Show, Generic)
+  deriving newtype (ToJSON, FromJSON, ToSchema, Eq, Haskell.Eq, Haskell.Ord)
+PlutusTx.makeIsDataIndexed ''Coin [('Coin, 0)]
+PlutusTx.makeLift ''Coin
+
+newtype Amount a = Amount { unAmount :: Integer }
+  deriving stock   (Haskell.Show, Generic)
+  deriving newtype (ToJSON, FromJSON, ToSchema, Eq, Ord, PrintfArg)
+  deriving newtype (Haskell.Eq, Haskell.Ord, Haskell.Num)
+  deriving newtype (AdditiveGroup, AdditiveMonoid, AdditiveSemigroup, MultiplicativeSemigroup)
+PlutusTx.makeIsDataIndexed ''Amount [('Amount, 0)]
+PlutusTx.makeLift ''Amount
+
+amountOf :: Value -> Coin a -> Amount a
+amountOf v = Amount . assetClassValueOf v . unCoin
+
+outputAmountOf :: TxOut -> Coin a -> Integer
+outputAmountOf out = assetClassValueOf (txOutValue out) . unCoin
+
+isUnity :: Value -> Coin a -> Bool
+isUnity v c = amountOf v c == 1
+
+{-# INLINABLE mkCoin #-}
+mkCoin:: CurrencySymbol -> TokenName -> Coin a
+mkCoin c = Coin . assetClass c
