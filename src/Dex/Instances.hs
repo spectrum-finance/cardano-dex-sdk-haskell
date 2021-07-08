98db5754de564876e40a10cc25937ba75c9c7606
GusevTimofey <gusev.t.s1@gmail.com>
2021-07-08 21:52:22 +0300

    instance OperationOps added

--- src/Dex/Instances.hs
+++ src/Dex/Instances.hs
@@ -8,6 +8,16 @@ import qualified PlutusTx.Builtins                as Builtins
 import qualified Data.Set                         as Set
 import           Dex.Models
 import           Dex.Utils
+import           Dex.Models as ROp  (RedeemOpData(..))
+import           Dex.Models  as Models
+import           Dex.Models as ROp  (RedeemOpData(..))
+import           Dex.Models as DOp  (DepositOpData(..))
+import           Dex.Models
+import           Ledger                           hiding (txOutValue)
+import qualified PlutusTx.Builtins                as Builtins
+import           Plutus.V1.Ledger.Value as Value
+import qualified PlutusTx.AssocMap                as Map
+import           Plutus.V1.Ledger.Address
 
 instance OperationOps (Operation a) where
     getInputs (SwapOperation swapData) = undefined
@@ -28,9 +38,44 @@ instance OperationOps (Operation a) where
             userOutput = generateUserRedeemOutput redeemData pool
             poolOutput = generatePoolRedeemOutput redeemData pool
         in [userOutput, poolOutput]
-    checkPool (SwapOperation swapData) pool = undefined
-    checkPool (DepositOperation depositData) pool = undefined
-    checkPool (RedeemOperation redeemData) pool = undefined
+    checkPool (SwapOperation swapData) pool =
+        let currentPoolData = poolData pool
+            poolValue = Models.txOutValue $ fullTxOut pool
+            isCorrectPoolId = poolId currentPoolData == swapPoolId swapData
+            isCorrectXTokenSymbol = inputtokenXNameTokenName swapData == tokenYName currentPoolData
+            poolXTokenValue = valueOf poolValue (CurrencySymbol $ tokenLPSymbol currentPoolData) (TokenName $ tokenLPName currentPoolData)
+            poolYTokenValue = valueOf poolValue (CurrencySymbol $ tokenXSymbol currentPoolData) (TokenName $ tokenXName currentPoolData)
+            isCorrectXAmount = poolXTokenValue - (minOutputTokenValue swapData) > 0
+            isCorrectYAmount = poolYTokenValue - (minOutputTokenValue swapData) > 0
+        in (isCorrectXAmount || isCorrectYAmount) && ((isCorrectXTokenSymbol && isCorrectXTokenName) || (isCorrectYTokenSymbol && isCorrectYTokenName))
+    checkPool (DepositOperation depositData) pool =
+        let currentPoolData = poolData pool
+            poolValue = Models.txOutValue $ fullTxOut pool
+            isCorrectXTokenSymbol = inputTokenXSymbol depositData == tokenXSymbol currentPoolData
+            isCorrectXTokenName = inputTokenXName depositData == tokenLPName currentPoolData
+            isCorrectYTokenSymbol = inputTokenYSymbol depositData == tokenYSymbol currentPoolData
+            isCorrectYTokenName = inputTokenYName depositData == tokenYName currentPoolData
+            isCorrectPoolId = poolId currentPoolData == depositPoolId depositData
+            reward = rewardLP depositData pool
+            poolLPTokenValue = valueOf poolValue (CurrencySymbol $ tokenLPSymbol currentPoolData) (TokenName $ tokenLPName currentPoolData)
+            isCorrectAmount = poolLPTokenValue - reward > 0
+        in isCorrectXTokenSymbol && isCorrectXTokenName && isCorrectYTokenSymbol && isCorrectYTokenName && isCorrectPoolId && isCorrectAmount
+
+    checkPool (RedeemOperation redeemData) pool =
+        let currentPoolData = poolData pool
+            poolValue = Models.txOutValue $ fullTxOut pool
+            redeemValue = Models.txOutValue $ ROp.proxyBox redeemData
+            isCorrectLPTokenSymbol = lpTokenSymbol redeemData == tokenLPSymbol currentPoolData
+            isCorrectLPTokenName = lpTokenName redeemData == tokenLPName currentPoolData
+            isCorrectPoolId = poolId currentPoolData == redeemPoolId redeemData
+            poolLPTokenValue = valueOf poolValue (CurrencySymbol $ tokenLPSymbol currentPoolData) (TokenName $ tokenLPName currentPoolData)
+            poolXTokenValue = valueOf poolValue (CurrencySymbol $ tokenLPSymbol currentPoolData) (TokenName $ tokenLPName currentPoolData)
+            poolYTokenValue = valueOf poolValue (CurrencySymbol $ tokenXSymbol currentPoolData) (TokenName $ tokenXName currentPoolData)
+            redeemLPTokenValue = valueOf poolValue (CurrencySymbol $ tokenYSymbol currentPoolData) (TokenName $ tokenYName currentPoolData)
+            (shareX, shareY) = sharesLP redeemData pool
+            isCorrectXAmount = poolXTokenValue - shareX > 0
+            isCorrectYAmount = poolYTokenValue - shareY > 0
+        in isCorrectLPTokenSymbol && isCorrectLPTokenName && isCorrectXAmount && isCorrectYAmount && isCorrectPoolId
 
 checkPoolContainsToken :: Builtins.ByteString -> Builtins.ByteString -> PoolData -> Bool
 checkPoolContainsToken inputTokenSymbol inputTokenName pool = undefined
