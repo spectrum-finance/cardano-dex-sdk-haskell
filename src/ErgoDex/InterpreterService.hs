dd9e1e5100f6745bbf87c5ae4d4674ce5c468185
GusevTimofey <gusev.t.s1@gmail.com>
2021-10-12 23:18:00 +0300

    InterpreterService draft

---
+++ src/ErgoDex/InterpreterService.hs
@@ -0,0 +1,18 @@
+module ErgoDex.InterpreterService
+    ( InterpreterService(..)
+    , mkInterpreterService
+    ) where
+
+import ErgoDex.Amm.Orders
+import ErgoDex.Amm.Pool
+import Plutus.V1.Ledger.Tx
+
+data Err
+data InterpreterService = InterpreterService
+    { swap    :: OrderAction Swap    -> Pool -> Either Err Tx
+    , deposit :: OrderAction Deposit -> Pool -> Either Err Tx
+    , redeem  :: OrderAction Redeem  -> Pool -> Either Err Tx
+    }
+
+mkInterpreterService :: InterpreterService
+mkInterpreterService = undefined
