40da78a3eba0487b366f38c67f0b973da41ed487
GusevTimofey <gusev.t.s1@gmail.com>
2021-10-12 20:46:32 +0300

    toJson/fromJson added

--- src/ErgoDex/Amm/Types.hs
+++ src/ErgoDex/Amm/Types.hs
@@ -2,5 +2,6 @@ module ErgoDex.Amm.Types where
 
 import Prelude
 import ErgoDex.Contracts.Types
+import Playground.Contract (FromJSON, ToJSON, Generic)
 
-newtype PoolId = PoolId { unPoolId :: Coin Nft } deriving (Show, Eq)
+newtype PoolId = PoolId { unPoolId :: Coin Nft } deriving (Show, Eq, Generic, FromJSON, ToJSON)
