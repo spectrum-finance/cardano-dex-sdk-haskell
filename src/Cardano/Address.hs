29b000417d532dc18a39d5cbe1aff824205d2e16
oskin1 <ilya.arcadich@gmail.com>
2021-10-18 15:31:50 +0300

    Fixes according to review.

--- src/Cardano/Address.hs
+++ src/Cardano/Address.hs
@@ -1,17 +1,17 @@
 module Cardano.Address
-  ( mkScriptAddress
+  ( renderToShellyAddress
   ) where
 
-import qualified Ledger.Typed.Scripts as Scripts
+import qualified Ledger.Typed.Scripts     as Scripts
 import qualified Plutus.V1.Ledger.Scripts as PlutusScripts
-import qualified Ledger.Scripts as LedgerScripts
-import qualified Cardano.Api as Script
-import Data.Text
+import qualified Ledger.Scripts           as LedgerScripts
+import qualified Cardano.Api              as Script
+import           Data.Text
 
-mkScriptAddress :: forall a . Scripts.TypedValidator a -> Text
-mkScriptAddress validatorInstance =
-    (Script.serialiseAddress . Script.makeShelleyAddress Script.Mainnet paymentCredential) Script.NoStakeAddress
-    where
-      validatorScript   = PlutusScripts.unValidatorScript $ Scripts.validatorScript validatorInstance
-      hashScript        = Script.hashScript $ LedgerScripts.toCardanoApiScript validatorScript
-      paymentCredential = Script.PaymentCredentialByScript hashScript
+renderToShellyAddress :: Script.NetworkId -> forall a . Scripts.TypedValidator a -> Text
+renderToShellyAddress network validatorInstance =
+    (Script.serialiseAddress . Script.makeShelleyAddress network paymentCredential) Script.NoStakeAddress
+  where
+    validatorScript   = PlutusScripts.unValidatorScript $ Scripts.validatorScript validatorInstance
+    hashScript        = Script.hashScript $ LedgerScripts.toCardanoApiScript validatorScript
+    paymentCredential = Script.PaymentCredentialByScript hashScript
