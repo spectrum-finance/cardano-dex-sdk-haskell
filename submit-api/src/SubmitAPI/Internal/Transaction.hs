a6fc13733c6426a395f9ea9a898df8e6da1f0741
GusevTimofey <gusev.t.s1@gmail.com>
2022-01-13 13:16:04 +0300

    debug

--- submit-api/src/SubmitAPI/Internal/Transaction.hs
+++ submit-api/src/SubmitAPI/Internal/Transaction.hs
@@ -59,8 +59,13 @@ buildTxBodyContent
   -> Sdk.TxCandidate
   -> f (TxBodyContent BuildTx AlonzoEra)
 buildTxBodyContent protocolParams network collateral Sdk.TxCandidate{..} = do
+  liftIO $ print "<---0>"
   txIns           <- buildTxIns $ Set.elems txCandidateInputs
+  liftIO $ print "<---1>"
+  liftIO $ print txIns
   txInsCollateral <- buildTxCollateral $ Set.elems collateral
+  liftIO $ print "<---2>"
+  liftIO $ print txInsCollateral
   txOuts          <- buildTxOuts network txCandidateOutputs
   liftIO $ print "<-->"
   liftIO $ print $ txOuts
