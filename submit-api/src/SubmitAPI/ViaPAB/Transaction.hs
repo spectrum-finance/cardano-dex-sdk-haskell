392d8a0c515613882af7ddfb9ffabc11f687747f
oskin1 <ilya.arcadich@gmail.com>
2022-01-15 14:28:23 +0100

    WIP: PAB backed SubmitAPI.

---
+++ submit-api/src/SubmitAPI/ViaPAB/Transaction.hs
@@ -0,0 +1,55 @@
+module SubmitAPI.ViaPAB.Transaction where
+
+import qualified Data.Set            as Set
+import qualified Data.Map            as Map
+import           Data.Functor
+
+import qualified CardanoTx.Models            as Sdk
+import qualified Ledger                      as P
+import qualified Ledger.Constraints          as P
+import qualified Ledger.Constraints.OffChain as P
+import           Ledger.Constraints.OffChain (UnbalancedTx(unBalancedTxTx))
+import qualified Plutus.V1.Ledger.Credential as P
+
+import CardanoTx.ToPlutus (ToPlutus(toPlutus))
+
+mkUnbalancedTx :: Set.Set Sdk.FullCollateralTxIn -> Sdk.TxCandidate -> P.UnbalancedTx
+mkUnbalancedTx collateral tx@Sdk.TxCandidate{..} =
+    P.UnbalancedTx
+      { unBalancedTxTx                  = mkPlutusTx collateral tx
+      , unBalancedTxRequiredSignatories = collectKeys allInputs
+      , unBalancedTxUtxoIndex           = Map.fromList $ allInputs <&> (\fout -> (Sdk.fullTxOutRef fout, toPlutus fout))
+      , unBalancedTxValidityTimeRange   = P.always -- todo
+      }
+  where
+    inputs    = Set.elems txCandidateInputs <&> Sdk.fullTxInTxOut
+    cinputs   = Set.elems collateral <&> Sdk.fullCollateralTxInTxOut
+    allInputs = inputs ++ cinputs
+
+mkPlutusTx :: Set.Set Sdk.FullCollateralTxIn -> Sdk.TxCandidate -> P.Tx
+mkPlutusTx collateral Sdk.TxCandidate{..} =
+  P.Tx
+    { txInputs      = Set.fromList $ Set.elems txCandidateInputs <&> toPlutus
+    , txCollateral  = Set.fromList $ Set.elems collateral <&> toPlutus
+    , txOutputs     = txCandidateOutputs <&> toPlutus
+    , txMint        = Sdk.unMintValue txCandidateValueMint
+    , txFee         = mempty
+    , txValidRange  = txCandidateValidRange
+    , txMintScripts = Sdk.mintInputsPolicies txCandidateMintInputs
+    , txSignatures  = mempty
+    , txRedeemers   = mempty -- todo
+    , txData        = collectOutputsData txCandidateOutputs
+    }
+
+collectKeys :: [Sdk.FullTxOut] -> Map.Map P.PubKeyHash (Maybe P.PubKey)
+collectKeys inputs = Map.fromList $ inputs >>= extractPkh
+  where
+    extractPkh Sdk.FullTxOut{fullTxOutAddress=P.Address (P.PubKeyCredential pkh) _} = [(pkh, Nothing)]
+    extractPkh _ = []
+
+collectOutputsData :: [Sdk.TxOutCandidate] -> Map.Map P.DatumHash P.Datum
+collectOutputsData outputs =
+    Map.fromList $ outputs >>= tryGetDatum
+  where
+    tryGetDatum Sdk.TxOutCandidate{txOutCandidateDatum=Just d} = pure (P.datumHash d, d)
+    tryGetDatum _                                              = mempty
