4231d07267bf10ccb7c7e465ea0b0bbe3ef616b8
oskin1 <ilya.arcadich@gmail.com>
2022-03-22 19:38:43 +0100

    Strict UTxO selection.

--- wallet-api/src/WalletAPI/Utxos.hs
+++ wallet-api/src/WalletAPI/Utxos.hs
@@ -19,14 +19,22 @@ import qualified Explorer.Models      as Explorer
 import qualified Explorer.Class       as Explorer
 
 data WalletOutputs f = WalletOutputs
-  { selectUtxos :: Value -> f (Maybe (Set.Set FullTxOut))
+  -- Select UTxOs satisfying the given minumal Value.
+  { selectUtxos       :: Value -> f (Maybe (Set.Set FullTxOut))
+  -- Assets other than present in the given minimal Value are not allowed.
+  , selectUtxosStrict :: Value -> f (Maybe (Set.Set FullTxOut))
   }
 
-mkWalletOutputs :: MonadIO f => Explorer f -> UtxoStore f -> Hash PaymentKey -> WalletOutputs f
-mkWalletOutputs explorer ustore pkh = WalletOutputs $ selectUtxos'' explorer ustore pkh
+mkWalletOutputs :: MonadIO f => Explorer f -> Hash PaymentKey -> f (WalletOutputs f)
+mkWalletOutputs explorer pkh = do
+  ustore <- mkUtxoStore
+  pure $ WalletOutputs
+    { selectUtxos       = selectUtxos'' explorer ustore pkh True
+    , selectUtxosStrict = selectUtxos'' explorer ustore pkh False
+    }
 
-selectUtxos'' :: Monad f => Explorer f -> UtxoStore f -> Hash PaymentKey -> Value -> f (Maybe (Set.Set FullTxOut))
-selectUtxos'' explorer@Explorer{..} ustore@UtxoStore{..} pkh requiredValue = do
+selectUtxos'' :: Monad f => Explorer f -> UtxoStore f -> Hash PaymentKey -> Bool -> Value -> f (Maybe (Set.Set FullTxOut))
+selectUtxos'' explorer@Explorer{..} ustore@UtxoStore{..} pkh strict requiredValue = do
   let
     fetchUtxos offset limit = do
       let
@@ -40,15 +48,21 @@ selectUtxos'' explorer@Explorer{..} ustore@UtxoStore{..} pkh requiredValue = do
       then fetchUtxos (offset + limit) limit
       else pure ()
 
+    extractAssets v = Set.fromList (flattenValue v <&> (\(cs, tn, _) -> (cs, tn)))
+    requiredAssets = extractAssets requiredValue
+
     collect :: [FullTxOut] -> Value -> [FullTxOut] -> Maybe [FullTxOut]
     collect acc valueAcc outs =
       case outs of
         fout@FullTxOut{..} : tl | valueAcc `lt` requiredValue ->
-          if Set.null $ Set.intersection (extractAssets fullTxOutValue) (extractAssets requiredValue)
-          then collect acc valueAcc tl -- current output doesn't contain the required asset at all, so skipping it
-          else collect (fout : acc) (fullTxOutValue <> valueAcc) tl -- need more outputs
+          if satisfies
+          then collect (fout : acc) (fullTxOutValue <> valueAcc) tl
+          else collect acc valueAcc tl -- current output doesn't contain the required asset at all, so skipping it
           where
-            extractAssets v = Set.fromList (flattenValue v <&> (\(cs, tn, _) -> (cs, tn)))
+            assets              = extractAssets fullTxOutValue
+            containsTargetAsset = not $ Set.null $ Set.intersection assets requiredAssets
+            containsOtherAssets = Set.null $ Set.difference assets requiredAssets
+            satisfies           = (containsTargetAsset && not strict) || (containsTargetAsset && not containsOtherAssets)
         [] | valueAcc `lt` requiredValue ->
           Nothing
         _ ->
@@ -57,5 +71,5 @@ selectUtxos'' explorer@Explorer{..} ustore@UtxoStore{..} pkh requiredValue = do
   utxos <- getUtxos
   case collect [] mempty (Set.elems utxos) of
     Just outs -> pure $ Just $ Set.fromList outs
-    Nothing   -> fetchUtxos 0 batchSize >> selectUtxos'' explorer ustore pkh requiredValue
+    Nothing   -> fetchUtxos 0 batchSize >> selectUtxos'' explorer ustore pkh strict requiredValue
       where batchSize = 20
