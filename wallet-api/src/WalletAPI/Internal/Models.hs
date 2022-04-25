95c6a27258103914ff54de1fd280938ae05a6409
oskin1 <ilya.arcadich@gmail.com>
2022-04-25 12:29:50 +0200

    Newtype FromDhall instances.

--- wallet-api/src/WalletAPI/Internal/Models.hs
+++ wallet-api/src/WalletAPI/Internal/Models.hs
@@ -1,16 +1,8 @@
 module WalletAPI.Internal.Models where
 
-import           RIO
-import qualified Data.ByteString         as BS
-import qualified Data.ByteArray          as BA
-import qualified Data.ByteString.Base16  as Hex
-import           Data.Aeson
-import           Data.Text (Text)
-import qualified Data.Text.Encoding      as T
-import           Data.Either.Combinators
-import           GHC.Generics            (Generic)
+import RIO
+import Data.Aeson
 
-import Crypto.Cipher.Types       (IV, BlockCipher, makeIV)
 import WalletAPI.Internal.Crypto
 
 data TrustStoreFile = TrustStoreFile
