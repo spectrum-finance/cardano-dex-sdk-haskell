4214db681cadbee6d44e758ae5bb169a388ae15b
Bromel777 <alexbromel@gmail.com>
2022-06-21 22:33:03 +0200

    add emptypool error

--- dex-core/src/ErgoDex/Amm/PoolActions.hs
+++ dex-core/src/ErgoDex/Amm/PoolActions.hs
@@ -24,12 +24,14 @@ import           ErgoDex.Amm.Pool
 import qualified ErgoDex.Contracts.Pool        as P
 import qualified ErgoDex.Contracts.Proxy.Order as O
 import           ErgoDex.Contracts.Types
-import           ErgoDex.PValidators
+import ErgoDex.PValidators
+    ( depositValidator, poolValidator, redeemValidator, swapValidator )
 import           CardanoTx.Models
 
 data OrderExecErr
   = PriceTooHigh
   | PoolMismatch PoolId PoolId
+  | EmptyPool PoolId
   deriving (Show)
 
 instance Exception OrderExecErr
