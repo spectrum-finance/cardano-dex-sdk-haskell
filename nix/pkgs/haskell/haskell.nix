fc1e7ee73d196a646ba474d8c766ac89133e5d74
Bromel777 <alexbromel@gmail.com>
2022-10-18 15:04:22 +0200

    contracts update

--- nix/pkgs/haskell/haskell.nix
+++ nix/pkgs/haskell/haskell.nix
@@ -43,7 +43,7 @@ let
       "https://github.com/input-output-hk/cardano-ledger"."c7c63dabdb215ebdaed8b63274965966f2bf408f" = "zTQbMOGPD1Oodv6VUsfF6NUiXkbN8SWI98W3Atv4wbI=";
       "https://github.com/input-output-hk/plutus-apps"."593ffafa59dd30ad28cfaf144c526c66328595d2" = "CIuI/Nz7O67ljOHDg7UBbXgWuIE7VPRdPX4VK0/DI3A=";
       "https://github.com/input-output-hk/hedgehog-extras"."714ee03a5a786a05fc57ac5d2f1c2edce4660d85" = "6KQFEzb9g2a0soVvwLKESEbA+a8ygpROcMr6bkatROE=";
-      "https://github.com/ergolabs/cardano-dex-contracts"."c7bdacfdf0f524d41240836e6dbdf3c7ada80c9d" = "xnUTvepJatCzUuGNx/GL4EmEFZPknezvx4O5yPbxnG4=";
+      "https://github.com/ergolabs/cardano-dex-contracts"."491e48b4ff1f261d3bc53e76371e4f07ba696a89" = "Xj1SP+eh8ONW0N0wdVKwHakY4slH7XoitznOuiDmR08=";
       "https://github.com/ergolabs/hlog"."19dfa3a6e696a3f63fc3539cd6b7a3fc4d999853" = "Lvmj1oLuXmktrboXh/BrXqLPf8FxSCXIf99GnBXu0Bk=";
     };
 
